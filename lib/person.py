4e370a5dddd8ed1cba64f9c58d5b0ef27ae83f60
Era-smus <erasmussaat1@gmail.com>
2025-11-18 00:58:38 +0300

    Modified dog.ppy

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+# class Person:
+#     # Class body goes here
+
+#     #Instance method definition
+#     talk = Person():
+#         def greet(self)
+#             print"Hello World"
+
 class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
+        
+# talk = Person()
+# fido.bark()
+
+# snoopy = Dog()
+# snoopy.bark()
